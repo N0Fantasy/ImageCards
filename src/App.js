966e8e419b62d5a6f3b676ca7dc3c5f14b5d03d9
N0Fantasy <new-e-box@mail.ru>
2022-06-29 12:25:57 +0300

    add loader

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { ImageCardList } from './components/ImageCardList/ImageCardList'
 import { OnlyLikesBtn } from './components/OnlyLikesBtn/OnlyLikesBtn'
 
+import loader from './assets/images/loader.gif'
 import { useImages } from './hooks/useImages'
 import { useDispatch } from 'react-redux'
 import { addImg } from './redux/actionCreators'
@@ -11,17 +12,22 @@ function App() {
   const data = useImages()
   const dispatch = useDispatch(addImg)
 
-  if (!data)
-    return null
+  if (!data) {
+    return (
+      <div className='LoaderContainer'>
+        <img className='Loader' src={loader}  alt='loader' />
+      </div>
+    )
+  }
 
   const upgradedData = data.map(image => ({ ...image, liked: false }))
   dispatch(addImg(upgradedData))
 
   return (
-      <div className="App">
-        <OnlyLikesBtn />
-        <ImageCardList />
-      </div>
+    <div className="App">
+      <OnlyLikesBtn />
+      <ImageCardList />
+    </div>
   )
 }
 
